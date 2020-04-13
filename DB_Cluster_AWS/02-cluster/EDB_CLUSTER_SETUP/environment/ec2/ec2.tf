b9f59135ee3f264ce81b2e19f321fa2c7e989044
Doug Ortiz <doug.ortiz@enterprisedb.com>
2020-04-13 17:21:08 -0400

    Updated 02-cluster

--- DB_Cluster_AWS/02-cluster/EDB_CLUSTER_SETUP/environment/ec2/ec2.tf
+++ DB_Cluster_AWS/02-cluster/EDB_CLUSTER_SETUP/environment/ec2/ec2.tf
@@ -52,10 +52,11 @@ resource "aws_instance" "EDB_DB_Cluster" {
   count                  = length(var.subnet_id)
   ami                    = data.aws_ami.centos_ami.id
   instance_type          = var.instance_type
+  #key_name               = aws_key_pair.generated_sshkey.key_name
   key_name               = var.ssh_keypair
   subnet_id              = var.subnet_id[count.index]
   iam_instance_profile   = var.iam_instance_profile
-  vpc_security_group_ids = [var.custom_security_group_id == "" ? var.aws_security_group_edb_sg[0] : var.custom_security_group_id]
+  vpc_security_group_ids = [var.custom_security_group_id]
 
 
   root_block_device {
@@ -69,121 +70,18 @@ resource "aws_instance" "EDB_DB_Cluster" {
     Created_By = var.created_by
   }
 
-
-  provisioner "local-exec" {
-    command = "echo '${self.public_ip} ansible_ssh_private_key_file=${var.ssh_key_path}' >> ${path.module}/../../utilities/scripts/hosts"
-  }
-
-  provisioner "local-exec" {
-    command = "sleep 60"
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "yum info python"
-    ]
-    connection {
-      host        = self.public_ip
-      type        = "ssh"
-      user        = var.ssh_user
-      private_key = file(var.ssh_key_path)
-    }
-  }
-
-  provisioner "local-exec" {
-    command = "ansible-playbook -i ${path.module}/../../utilities/scripts/hosts -u centos --private-key '${file(var.ssh_key_path)}' '${path.module}/../../utilities/scripts/install${var.db_engine}.yml' --extra-vars='USER=${var.EDB_yumrepo_username} PASS=${var.EDB_yumrepo_password} PGDBUSER=${local.DBUSERPG}  EPASDBUSER=${local.DBUSEREPAS}' --limit ${self.public_ip}"
-  }
-
-  lifecycle {
-    create_before_destroy = true
-  }
-
 }
+
 
 resource "aws_eip" "master-ip" {
   instance = aws_instance.EDB_DB_Cluster[0].id
   vpc      = true
-
-  depends_on = [aws_instance.EDB_DB_Cluster[0]]
-
-  provisioner "local-exec" {
-    command = "echo '${aws_eip.master-ip.public_ip} ansible_ssh_private_key_file=${var.ssh_key_path}' >> ${path.module}/../../utilities/scripts/hosts"
-  }
 }
 
 
 locals {
-  DBUSERPG   = "${var.db_user == "" || var.db_engine == regexall("var.db_engine}", "pg10 pg11 pg12") ? "postgres" : var.db_user}"
+  DBUSERPG   = "${var.db_user == "" || var.db_engine == regexall("${var.db_engine}", "pg10 pg11 pg12") ? "postgres" : var.db_user}"
   DBUSEREPAS = "${var.db_user == "" || var.db_engine == regexall("${var.db_engine}", "epas10 eaps11 epas12") ? "enterprisedb" : var.db_user}"
   DBPASS     = "${var.db_password == "" ? "postgres" : var.db_password}"
   REGION     = "${substr("${aws_instance.EDB_DB_Cluster[0].availability_zone}", 0, 9)}"
-}
-
-#####################################
-## Configuration of streaming replication start here
-#
-#
-########################################### 
-
-
-resource "null_resource" "configuremaster" {
-  # Define the trigger condition to run the resource block
-  triggers = {
-    cluster_instance_ids = "${join(",", aws_instance.EDB_DB_Cluster.*.id)}"
-  }
-
-  depends_on = [aws_instance.EDB_DB_Cluster[0]]
-
-
-
-  provisioner "local-exec" {
-    command = "ansible-playbook -i ${path.module}/../../utilities/scripts/hosts -u centos --private-key '${file(var.ssh_key_path)}' '${path.module}/../../utilities/scripts/configuremaster.yml' --extra-vars='ip1=${aws_instance.EDB_DB_Cluster[1].private_ip} ip2=${aws_instance.EDB_DB_Cluster[2].private_ip} ip3=${aws_instance.EDB_DB_Cluster[0].private_ip} REPLICATION_USER_PASSWORD=${var.replication_password} DB_ENGINE=${var.db_engine} PGDBUSER=${local.DBUSERPG}  EPASDBUSER=${local.DBUSEREPAS} REPLICATION_TYPE=${var.replication_type} DBPASSWORD=${local.DBPASS} S3BUCKET=${var.s3bucket}' --limit ${aws_eip.master-ip.public_ip}"
-
-  }
-
-}
-
-resource "null_resource" "configureslave1" {
-  # Define the trigger condition to run the resource block
-  triggers = {
-    cluster_instance_ids = "${join(",", aws_instance.EDB_DB_Cluster.*.id)}"
-  }
-
-  depends_on = [null_resource.configuremaster]
-
-  provisioner "local-exec" {
-    command = "ansible-playbook -i ${path.module}/../../utilities/scripts/hosts -u centos --private-key '${file(var.ssh_key_path)}' '${path.module}/../../utilities/scripts/configureslave.yml' --extra-vars='ip1=${aws_eip.master-ip.private_ip} ip2=${aws_instance.EDB_DB_Cluster[2].private_ip} REPLICATION_USER_PASSWORD=${var.replication_password} DB_ENGINE=${var.db_engine} REPLICATION_TYPE=${var.replication_type} SLAVE1=${aws_instance.EDB_DB_Cluster[1].public_ip} SLAVE2=${aws_instance.EDB_DB_Cluster[2].public_ip} MASTER=${aws_eip.master-ip.public_ip} S3BUCKET=${var.s3bucket}' --limit ${aws_instance.EDB_DB_Cluster[1].public_ip},${aws_eip.master-ip.public_ip}"
-
-  }
-
-}
-
-resource "null_resource" "configureslave2" {
-  # Define the trigger condition to run the resource block
-  triggers = {
-    cluster_instance_ids = "${join(",", aws_instance.EDB_DB_Cluster.*.id)}"
-  }
-
-  depends_on = [null_resource.configuremaster]
-
-  provisioner "local-exec" {
-    command = "ansible-playbook -i ${path.module}/../../utilities/scripts/hosts -u centos --private-key '${file(var.ssh_key_path)}' '${path.module}/../../utilities/scripts/configureslave.yml' --extra-vars='ip1=${aws_eip.master-ip.private_ip} ip2=${aws_instance.EDB_DB_Cluster[1].private_ip} REPLICATION_USER_PASSWORD=${var.replication_password} DB_ENGINE=${var.db_engine} REPLICATION_TYPE=${var.replication_type} SLAVE2=${aws_instance.EDB_DB_Cluster[2].public_ip} SLAVE1=${aws_instance.EDB_DB_Cluster[1].public_ip} MASTER=${aws_eip.master-ip.public_ip} S3BUCKET=${var.s3bucket}' --limit ${aws_instance.EDB_DB_Cluster[2].public_ip},${aws_eip.master-ip.public_ip}"
-
-  }
-
-}
-
-
-resource "null_resource" "removehostfile" {
-
-  provisioner "local-exec" {
-    command = "rm -rf ${path.module}/../../utilities/scripts/hosts"
-  }
-
-  depends_on = [
-    null_resource.configureslave2,
-    null_resource.configureslave1,
-    null_resource.configuremaster,
-    aws_instance.EDB_DB_Cluster
-  ]
 }
--- DB_Cluster_AWS/02-cluster/edb_cluster_input.tf
+++ DB_Cluster_AWS/02-cluster/edb_cluster_input.tf
@@ -2,7 +2,7 @@
 
 ### region_name
 ### instance_type
-### ssh_keypair   
+### ssh_keypair_file
 ### ssh_key_path
 ### subnet_id
 ### vpc_id 
@@ -53,10 +53,10 @@ module "edb-db-cluster" {
 
   # Enter this mandatory field which is VPC ID
   #vpc_id = ""
-  vpc_id = "vpc-07eda8ebfbf602baf"
+  vpc_id = "vpc-0fd2b4a8062a8481f"
 
   # Enter subnet ID where instance going to create in format ["subnetid1","subnetid2", "subnetid3"]
-  subnet_id = ["subnet-0aaade8494c52c63a", "subnet-081ea42891ac9af6b", "subnet-09a1c944af4700da9"]
+  subnet_id = ["subnet-005115533b627ad95", "subnet-04c8a12bccbcaf065", "subnet-0e5197d0def3c85e6"]
 
   # Enter AWS Instance type like t2.micro, t3.large, c4.2xlarge m5.2xlarge etc....
   # instance_type = "t2.micro"
@@ -70,19 +70,19 @@ module "edb-db-cluster" {
 
   # Enter AWS VPC Security Group ID. 
   # If left blank new security group will create and attached to newly created instance ...
-  custom_security_group_id = ""
+  custom_security_group_id = "sg-06cf8d22f9c9e0bb2"
 
   # Provide s3 bucket name followed by folder name for wal archive. Eg. s3bucket=bucketname/foldername
-  #s3bucket = ""
-  s3bucket = "edb-postgres/wal"
+  #s3bucket = "s3bucket="
+  s3bucket = "s3bucket=edb-postgres/wal"
 
   # Enter SSH key pair name. You must create this before running this terraform config file
   #ssh_keypair = ""
-  ssh_keypair = "/home/dortiz/test-edb-deployment-keypair.pem"
+  ssh_keypair = "edb-postgres-cluster"
 
   # Provide path of ssh private file downloaded on your local system.
   #ssh_key_path = ""
-  ssh_key_path = "/home/dortiz/"
+  ssh_key_path = "/home/dortiz/edb-postgres-cluster.pem"
 
   # Select database engine(DB) version like pg10-postgresql version10, epas12-Enterprise Postgresql Advanced server etc.
   # DB version support V10-V12
