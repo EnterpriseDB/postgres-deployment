7946182220839b7d17973d8cfde156772fc3560c
Doug Ortiz <doug.ortiz@enterprisedb.com>
2020-08-13 11:20:48 -0700

    Updated DB_Cluster_AWS

--- DB_Cluster_AWS/01-prereqs/global/s3/buckets.tf
+++
@@ -1,8 +0,0 @@
-variable aws_bucket_name {}
-variable project_tags {}
-
-resource "aws_s3_bucket" "edb-bucket" {
-  bucket = var.aws_bucket_name
-  acl    = "private"
-  tags   = var.project_tags
-}
--- DB_Cluster_AWS/02-cluster/EDB_CLUSTER_SETUP/environment/security/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "aws_security_group_edb_sg" {
-  value = aws_security_group.edb_sg.*.id
-}
