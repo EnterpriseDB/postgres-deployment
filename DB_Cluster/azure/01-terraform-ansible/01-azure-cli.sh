f3cd551ec515d121fefbd3bf7fb9b5f350b3c010
Doug Ortiz <doug.ortiz@enterprisedb.com>
2020-08-25 10:56:27 -0700

    Added support for Debian 9, RHEL 7, CentOS 7 and Ubuntu 20.04

--- DB_Cluster/azure/01-terraform-ansible/01-azure-cli.sh
+++ DB_Cluster/azure/01-terraform-ansible/01-azure-cli.sh
@@ -6,19 +6,48 @@
 #curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 
 # Install dependent packages
-sudo apt-get update
-sudo apt-get -y install ca-certificates curl apt-transport-https lsb-release gnupg
+if cat /etc/*release | grep ^NAME | grep Ubuntu | grep Debian; then
+   sudo apt-get -y update
+   sudo apt-get -y install ca-certificates curl apt-transport-https lsb-release gnupg
+fi
+
+if cat /etc/*release | grep ^NAME | grep CentOS | grep Red; then
+   sudo yum update -y
+   sudo yum install ca-certificates curl apt-transport-https lsb-release gnupg -y
+fi
 
 # Download Microsoft signing key
-curl -sL https://packages.microsoft.com/keys/microsoft.asc |
-    gpg --dearmor |
-    sudo tee /etc/apt/trusted.gpg.d/microsoft.gpg > /dev/null
+if cat /etc/*release | grep ^NAME | grep Ubuntu | grep Debian; then
+   curl -sL https://packages.microsoft.com/keys/microsoft.asc |
+      gpg --dearmor |
+      sudo tee /etc/apt/trusted.gpg.d/microsoft.gpg > /dev/null
+fi
+
+if cat /etc/*release | grep ^NAME | grep CentOS | grep Red; then
+   sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+fi
 
 # Add Azure CLI Repository
 AZ_REPO=$(lsb_release -cs)
-echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" |
-    sudo tee /etc/apt/sources.list.d/azure-cli.list
+if cat /etc/*release | grep ^NAME | grep Ubuntu | grep Debian; then
+   echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
+fi
+
+if cat /etc/*release | grep ^NAME | grep CentOS | grep Red; then
+   sudo sh -c 'echo -e "[azure-cli]
+     name=Azure CLI
+     baseurl=https://packages.microsoft.com/yumrepos/azure-cli
+     enabled=1
+     gpgcheck=1
+     gpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/azure-cli.repo'
+fi
 
 # Install Azure CLI
-sudo apt-get -y update
-sudo apt-get -y install azure-cli
+if cat /etc/*release | grep ^NAME | grep Ubuntu | grep Debian; then
+   sudo apt-get -y update
+   sudo apt-get -y install azure-cli
+fi
+
+if cat /etc/*release | grep ^NAME | grep CentOS | grep Red; then
+   sudo yum install azure-cli -y
+fi
