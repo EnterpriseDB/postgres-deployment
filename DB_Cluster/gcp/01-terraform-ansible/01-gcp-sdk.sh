c799ddff5b27e4b95faeea0ef20f7d1a6e8bf31c
dougortiz <63114888+dougortiz@users.noreply.github.com>
2020-08-28 17:42:22 -0400

    Update 01-gcp-sdk.sh

--- DB_Cluster/gcp/01-terraform-ansible/01-gcp-sdk.sh
+++ DB_Cluster/gcp/01-terraform-ansible/01-gcp-sdk.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
 # GCP SDK Installation
-sudo curl https://sdk.cloud.google.com/ > install.sh
 sudo rm -r ~/google-cloud-sdk
+sudo curl https://sdk.cloud.google.com/ > install.sh
 bash install.sh --disable-prompts
-source ~/.bashrc
+export PATH=~/google-cloud-sdk/bin:$PATH
 gcloud init
