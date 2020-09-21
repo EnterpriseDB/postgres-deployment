9fd6b314724df5aa74f026bdc1d3fb77242542df
Vibhor Kumar <vibhor.kumar@enterprisedb.com>
2020-09-21 19:06:02 +0000

    Cleaning up the old directories

--- Expand_DB_Cluster_AWS/EDB_ADD_REPLICA/output.tf
+++
@@ -1,4 +0,0 @@
-output "Slave-PublicIP" {
-value = aws_instance.EDB_Expand_DBCluster.public_ip
-}
-
--- Expand_DB_Cluster_AWS/EDB_ADD_REPLICA/utilities/scripts/failover.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-MEIP=eip
-
-id=`curl -s http://169.254.169.254/latest/meta-data/instance-id`
-
-aws ec2 associate-address --instance-id "$id" --public-ip "$MEIP" --allow-reassociation --region region-name
