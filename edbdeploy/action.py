2f33c74bf631e81e7371743622b92a1aeda3d56d
Julien Tachoires <julien.tachoires@enterprisedb.com>
2021-04-11 18:45:35 +0200

    Add spinning cursor when executing actions

--- edbdeploy/action.py
+++ edbdeploy/action.py
@@ -1,16 +1,58 @@
 import sys
 import logging
+import time
+import threading
+
+
+def spinning_cursor():
+    """
+    Generator returning chars used by the spinner.
+    """
+    while True:
+        for cursor in '|/-\\':
+            yield cursor
+
+
+class Spinner(threading.Thread):
+    """
+    Terminal spinner running in its own thread.
+    """
+    def __init__(self, *args, **kwargs):
+        super(Spinner, self).__init__(*args, **kwargs)
+        self._stop = threading.Event()
+
+    def stop(self):
+        self._stop.set()
+        sys.stdout.flush()
+
+    def run(self):
+        cursor = spinning_cursor()
+        while True:
+            time.sleep(0.1)
+            if self._stop.isSet():
+                return
+            sys.stdout.write(next(cursor))
+            sys.stdout.flush()
+            sys.stdout.write('\b')
+
 
 class ActionManager:
     def __init__(self, msg):
         self.msg = msg
+        self.spinner = Spinner()
 
     def __enter__(self):
         logging.info(self.msg)
         sys.stdout.write("%s ... " % self.msg)
         sys.stdout.flush()
 
+        # Start the spinner
+        self.spinner.start()
+
     def __exit__(self, exc_type, exc_value, traceback):
+        # Stop the spinner
+        self.spinner.stop()
+
         if exc_type == None:
             sys.stdout.write("\033[1m\033[92mok\033[0m\n")
         elif exc_type == KeyboardInterrupt:
