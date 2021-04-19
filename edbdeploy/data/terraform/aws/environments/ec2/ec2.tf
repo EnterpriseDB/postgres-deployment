7c90221c3393afe5b02fc7a7485d09af891ec439
Doug Ortiz <doug.ortiz@enterprisedb.com>
2021-04-19 05:46:32 -0700

    PER-42v3

--- edbdeploy/data/terraform/aws/environments/ec2/ec2.tf
+++ edbdeploy/data/terraform/aws/environments/ec2/ec2.tf
@@ -532,7 +532,7 @@ resource "null_resource" "pg-set-hostname" {
         "/tmp/add_ips_to_hosts.sh ${var.cluster_name} barman ${join(" ", aws_instance.barman_server.*.private_ip)} >> /tmp/barman_hostnames.log 2>&1",
         "/tmp/add_ips_to_hosts.sh ${var.cluster_name} pooler ${join(" ", aws_instance.pooler_server.*.private_ip)} >> /tmp/pooler_hostnames.log 2>&1",
         "chmod a+x /tmp/add_nickname_to_shell.sh",        
-        "/tmp/add_nickname_to_shell.sh ${var.cluster_name} postgres ${join(" ", aws_instance.postgres_server.*.private_ip)} >> /tmp/pem_nicknames.log 2>&1"
+        "/tmp/add_nickname_to_shell.sh ${var.cluster_name} postgres ${join(" ", aws_instance.postgres_server.*.private_ip)} >> /tmp/postgres_nicknames.log 2>&1"
     ]
 
     connection {
