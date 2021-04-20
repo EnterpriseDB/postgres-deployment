51cd70a8693e29f170cce1e710a5903277dae692
Mark Wong <mark.wong@enterprisedb.com>
2021-04-20 15:27:35 -0700

    Separate Azure Virtual Machine module for HammerDB benchmarking
    In order to use Ultra SSD storage, we need additional attributes defined
    and different caching attributes.

--- edbdeploy/data/terraform/azure/environments/storagecontainer/storagecontainer.tf
+++ edbdeploy/data/terraform/azure/environments/storagecontainer/storagecontainer.tf
@@ -1,8 +1,10 @@
 variable "storageaccount_name" {}
 variable "storagecontainer_name" {}
+variable "hammerdb" {}
 
 
 resource "azurerm_storage_container" "container" {
+  count = var.hammerdb ? 0 : 1
   name                  = var.storagecontainer_name
   storage_account_name  = var.storageaccount_name
   container_access_type = "private"
