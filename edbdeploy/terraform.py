eade4eaac5c5a5a647fda87adee35e005e4748e1
Doug Ortiz <doug.ortiz@enterprisedb.com>
2021-07-02 09:55:03 -0700

    Added support for Terraform v1.0.1

--- edbdeploy/terraform.py
+++ edbdeploy/terraform.py
@@ -23,7 +23,7 @@ class TerraformCli:
         self.environ['TF_PLUGIN_CACHE_DIR'] = self.plugin_cache_dir
         # Terraform supported version interval
         self.min_version = (1, 0, 0)
-        self.max_version = (1, 0, 0)
+        self.max_version = (1, 0, 1)
         # Path to look up for executable
         self.bin_path = None
         # Force Terraform binary path if bin_path exists and contains
