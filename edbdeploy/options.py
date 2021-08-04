da3b6abaa09ae51b5e43ef79730cda51cdc69a6c
vincentp7212 <86248040+vincentp7212@users.noreply.github.com>
2021-08-04 13:06:12 -0500

    EFM 4.2 works with Virtualbox

--- edbdeploy/options.py
+++ edbdeploy/options.py
@@ -128,10 +128,10 @@ class EFMVersionOptionVMWare:
     """)
 
 class EFMVersionOptionVirtualBox:
-    choices = ['3.10', '4.0', '4.1']
+    choices = ['3.10', '4.0', '4.1', '4.2']
     default = '4.2'
     help = textwrap.dedent("""
-        EDB Failover Manager version. Allowed values are: 3.10, 4.0, 4.1.
+        EDB Failover Manager version. Allowed values are: 3.10, 4.0, 4.1 and 4.2.
         Default: %(default)s
     """)
 
