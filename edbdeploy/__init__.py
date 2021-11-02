25b9fc39e613c8d8f5535d5441a2468a47099cc7
Julien Tachoires <julien.tachoires@enterprisedb.com>
2021-11-02 15:19:05 +0100

    Bump version to 3.7.0

--- edbdeploy/__init__.py
+++ edbdeploy/__init__.py
@@ -1,6 +1,6 @@
-__version__ = "3.7.0.dev1"
+__version__ = "3.7.0"
 # Version number of the Ansible collection we want to use
-__edb_ansible_version__ = "3.6.2"
+__edb_ansible_version__ = "3.7.1"
 
 def to_num(version):
     """
