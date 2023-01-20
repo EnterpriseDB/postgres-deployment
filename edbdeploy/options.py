cc5e484d4b44544b0f70803734d667ed2e97d2ec
Vibhor Kumar <Vibhor Kumar>
2023-01-20 12:06:14 -0500

    EFM 4.5 and EDB Ansible 3.16 support Adding support from EFM 4.5 and making this version default for all EDB-RA architectures Also made edb-ansible 3.16 as a default version

--- edbdeploy/options.py
+++ edbdeploy/options.py
@@ -97,26 +97,26 @@ class PgVersionOptionAzureDB:
 
 
 class EFMVersionOption:
-    choices = ['3.10', '4.0', '4.1', '4.2', '4.3', '4.4']
-    default = '4.4'
+    choices = ['3.10', '4.0', '4.1', '4.2', '4.3', '4.4', '4.5']
+    default = '4.5'
     help = textwrap.dedent("""
-        EDB Failover Manager version. Allowed values are: 3.10, 4.0, 4.1, 4.2, 4.3 and 4.4.
+        EDB Failover Manager version. Allowed values are: 3.10, 4.0, 4.1, 4.2, 4.3, 4.4 and 4.5.
         Default: %(default)s
     """)
 
 class EFMVersionOptionVMWare:
-    choices = ['3.10', '4.0', '4.1', '4.2', '4.3', '4.4']
-    default = '4.4'
+    choices = ['3.10', '4.0', '4.1', '4.2', '4.3', '4.4', '4.5']
+    default = '4.5'
     help = textwrap.dedent("""
-        EDB Failover Manager version. Allowed values are: 3.10, 4.0, 4.1, 4.2, 4.3 and 4.4.
+        EDB Failover Manager version. Allowed values are: 3.10, 4.0, 4.1, 4.2, 4.3, 4.4 and 4.5.
         Default: %(default)s
     """)
 
 class EFMVersionOptionVirtualBox:
-    choices = ['3.10', '4.0', '4.1', '4.2', '4.3', '4.4']
-    default = '4.4'
+    choices = ['3.10', '4.0', '4.1', '4.2', '4.3', '4.4', '4.5']
+    default = '4.5'
     help = textwrap.dedent("""
-        EDB Failover Manager version. Allowed values are: 3.10, 4.0, 4.1, 4.2, 4.3 and 4.4.
+        EDB Failover Manager version. Allowed values are: 3.10, 4.0, 4.1, 4.2, 4.3, 4.4 and 4.5.
         Default: %(default)s
     """)
 
