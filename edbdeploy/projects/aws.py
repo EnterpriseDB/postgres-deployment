51c5b5e5c9737102cde0e9e6c50f9551b4c69ce2
Bryan Barajas <bryan.barajas@enterprisedb.com>
2023-02-07 01:38:36 +0000

    AWS and initial implementation changes: * aws version updated * capture stdout from edbterraform to not pollute ActionManager output * tag servers with a type * fixes to make sure right terraform directory is used * Templates updated to use tagging from each resource that is created. Since ordering and priority is occasionally needed, resources can be tagged with priority or index if a set of similar machines need to be sorted for setup. * Generate templates previously handled by terraform   * add_host.sh   * inventory.yml   * ssh_config * Removed setup_hammerdbserver role as it is merged under setup_hammerdb now. See commit: https://github.com/EnterpriseDB/edb-ansible/commit/423f4f704c5217e3ef4a582612544becf0333cdd * updated security groups since some overrides will be needed

--- edbdeploy/projects/aws.py
+++ edbdeploy/projects/aws.py
@@ -4,22 +4,22 @@ from ..action import ActionManager as AM
 from ..cloud import CloudCli
 from ..errors import ProjectError
 from ..project import Project
-from ..render import build_inventory_yml
-
+from ..render import build_inventory_yml, build_ansible_inventory
 
 class AWSProject(Project):
-    def __init__(self, name, env, bin_path=None):
-        super(AWSProject, self).__init__('aws', name, env, bin_path)
+    def __init__(self, name, env, bin_path=None, using_edbterraform=True):
+        super(AWSProject, self).__init__('aws', name, env, bin_path, using_edbterraform)
 
     def hook_post_configure(self, env):
         # Hook function called by Project.configure()
-        # Transform Terraform templates
-        self._transform_terraform_tpl()
+        ######################################################
         # Build the vars files for Terraform and Ansible
-        self._build_terraform_vars_file(env)
-        self._build_ansible_vars_file(env)
+        # _build_terraform_vars override below
+        super()._build_terraform_files()
+        ######################################################
+        super()._build_ansible_vars_file(env)
         # Copy Ansible playbook into project dir.
-        self._copy_ansible_playbook()
+        super()._copy_ansible_playbook()
         # Check Cloud Instance type and Image availability.
         self._check_instance_image(env)
 
@@ -32,11 +32,22 @@ class AWSProject(Project):
     def hook_inventory_yml(self, vars):
         # Hook function called by Project.provision()
         with AM("Generating the inventory.yml file"):
-            build_inventory_yml(
-                self.ansible_inventory,
-                os.path.join(self.project_path, 'servers.yml'),
-                vars=vars
-            )
+            if self.using_edbterraform:
+                template_vars = dict()
+                template_vars['vars'] = vars
+                server_vars = super()._load_terraform_outputs()
+                server_vars = server_vars.get('servers')
+                template_vars['servers'] = server_vars.get('machines', {})
+                build_ansible_inventory(
+                    self.project_path,
+                    vars=template_vars
+                )
+            else:
+                build_inventory_yml(
+                    self.ansible_inventory,
+                    super()._get_servers_filepath(),
+                    vars=vars
+                )
 
     def _build_ansible_vars(self, env):
         # Overload Project._build_ansible_vars()
@@ -65,6 +76,30 @@ class AWSProject(Project):
             'additional_volumes': pg['additional_volumes'],
             'instance_type': pg['instance_type'],
         })
+
+        # set variables for use with edbterraform
+        cloud_cli = CloudCli(self.env.cloud, bin_path=self.cloud_tools_bin_path)
+        if not self.terraform_vars.get(self.cloud_provider):
+            self.terraform_vars[self.cloud_provider] = dict()
+        self.terraform_vars['created_by'] = cloud_cli.cli.get_caller_info()
+        # ports needed
+        self.terraform_vars['service_ports'], self.terraform_vars['region_ports'] = super()._get_default_ports()
+        self.terraform_vars['image'] = dict()
+        self.terraform_vars['image']['name'] = self.terraform_vars['aws_image']
+        self.terraform_vars['image']['owner'] = cloud_cli.cli.get_image_owner(self.terraform_vars['aws_image'], self.terraform_vars['aws_region'])
+        self.terraform_vars['image']['ssh_user'] = self.terraform_vars['ssh_user']
+        self.terraform_vars['region'] = self.terraform_vars['aws_region']
+        # create a set of zones from each machine's instance type and region availability zone  
+        instance_types: set = {
+            values['instance_type'] for key, values in self.terraform_vars.items()
+            if any(substr in key for substr in ['dbt2_client', 'dbt2_driver', '_server']) and
+            isinstance(values, dict) and
+            values.get('count', 0) >= 1
+            and values.get('instance_type')
+        }
+        filtered_zones: set = {zone for instance in instance_types for zone in cloud_cli.check_instance_type_availability(instance, self.terraform_vars['aws_region'])}
+        filtered_zones.intersection_update(cloud_cli.cli.get_available_zones(self.terraform_vars['aws_region']))
+        self.terraform_vars['zones'] = list(filtered_zones)
 
     def _check_instance_image(self, env):
         # Overload Project._check_instance_image()
@@ -103,7 +138,5 @@ class AWSProject(Project):
                         % (self.terraform_vars['aws_image'], env.aws_region)
                     )
             with AM("Updating Terraform vars with the AMI id %s" % aws_ami_id):
-                # Useless variable for Terraform
-                del(self.terraform_vars['aws_image'])
                 self.terraform_vars['aws_ami_id'] = aws_ami_id
-                self._save_terraform_vars()
+                super()._save_terraform_vars()
