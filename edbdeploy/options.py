508cc8d14f6305040df8db4bc487cb05a702e534
Vibhor Kumar <Vibhor Kumar>
2023-02-21 21:33:21 +0530

    Update for postgres-deployment This PR push the following changes 1. Bump the postgres-deployment version 2. Make postgres-deployment to use latest version of edb-ansible 3. Use the latest version of PEM 9.0.0 4. BDR POt has monitoring of HARP/ETCD

--- edbdeploy/options.py
+++ edbdeploy/options.py
@@ -97,26 +97,26 @@ class PgVersionOptionAzureDB:
 
 
 class EFMVersionOption:
-    choices = ['3.10', '4.0', '4.1', '4.2', '4.3', '4.4', '4.5']
-    default = '4.5'
+    choices = ['3.10', '4.0', '4.1', '4.2', '4.3', '4.4', '4.5', '4.6']
+    default = '4.6'
     help = textwrap.dedent("""
-        EDB Failover Manager version. Allowed values are: 3.10, 4.0, 4.1, 4.2, 4.3, 4.4 and 4.5.
+        EDB Failover Manager version. Allowed values are: 3.10, 4.0, 4.1, 4.2, 4.3, 4.4, 4.5 and 4.6.
         Default: %(default)s
     """)
 
 class EFMVersionOptionVMWare:
-    choices = ['3.10', '4.0', '4.1', '4.2', '4.3', '4.4', '4.5']
-    default = '4.5'
+    choices = ['3.10', '4.0', '4.1', '4.2', '4.3', '4.4', '4.5', '4.6']
+    default = '4.6'
     help = textwrap.dedent("""
-        EDB Failover Manager version. Allowed values are: 3.10, 4.0, 4.1, 4.2, 4.3, 4.4 and 4.5.
+        EDB Failover Manager version. Allowed values are: 3.10, 4.0, 4.1, 4.2, 4.3, 4.4, 4.5 and 4.6.
         Default: %(default)s
     """)
 
 class EFMVersionOptionVirtualBox:
-    choices = ['3.10', '4.0', '4.1', '4.2', '4.3', '4.4', '4.5']
-    default = '4.5'
+    choices = ['3.10', '4.0', '4.1', '4.2', '4.3', '4.4', '4.5', '4.6']
+    default = '4.6'
     help = textwrap.dedent("""
-        EDB Failover Manager version. Allowed values are: 3.10, 4.0, 4.1, 4.2, 4.3, 4.4 and 4.5.
+        EDB Failover Manager version. Allowed values are: 3.10, 4.0, 4.1, 4.2, 4.3, 4.4, 4.5 and 4.6.
         Default: %(default)s
     """)
 
